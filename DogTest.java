8640032706039cfcd5ac5de2a33fce0b5e8223af
KimVal <71520455+Valeria19k@users.noreply.github.com>
2020-12-20 22:06:31 +0300

    Update DogTest.java

--- DogTest.java
+++ DogTest.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class DogTest {
     public static void main(String[] args){
         Dog[] pets= new Dog[2];
-        pets[0] = new Dog("Frosya",2);
-        pets[1] = new Dog("Seva");
-        pets[1].setAge(19);
+        pets[0] = new Dog("Lili",6);
+        pets[1] = new Dog("Korjik");
+        pets[1].setAge(12);
         System.out.print(pets[1]);
         pets[0].intoHumanAge();
     }
